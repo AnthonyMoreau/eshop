413707af9704de57df52ebfbf7b3f5b79248e5ef
AnthonyMoreau <a82moreau@gmail.com>
2019-06-04 16:37:58 +0200

    dernier commit

--- assets/js/panier.js
+++ assets/js/panier.js
@@ -1,7 +1,8 @@
 let pr = document.querySelector('.pr')
 let tva = document.querySelector('.tva')
 let taxe = 0
-let button = document.querySelectorAll('button')
+let button = document.querySelectorAll('.ajouter')
+let retired = document.querySelectorAll('.retired')
 let totalPrice = 0
 let p = 0
 let quantite = 0
@@ -22,4 +23,20 @@ for(let i = 0; i < button.length; i++){
     })
 
 }
+for(let i = 0; i < retired.length; i++){
+
+    retired[i].addEventListener('click', function(e){
+        p = Number.parseInt(e.explicitOriginalTarget.value)
+        if(totalPrice !== 0){
+            totalPrice -= p
+            taxe = Math.round(totalPrice * (20.6/100))
+            quantite--
+            pr.innerText = "Prix : " + totalPrice  + ' Euros'
+            tva.innerText = "TVA : " + taxe  + ' Euros'
+            qt.innerText =' Quantité : ' + quantite
+        }
+    })
+
+}
+
 
